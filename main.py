6a8a22b0953964d8165350e5a19abb454d207207
lukemvu <luke.m.vu@gmail.com>
2022-04-15 23:44:41 -0400

    progress

--- main.py
+++ main.py
@@ -3,17 +3,48 @@ import cv2 as cv
 import numpy as np
 import os
 from time import time
-import pyautogui
-from PIL import ImageGrab
+import win32gui, win32ui, win32con
+
 
 # Change the working directory to the folde this script is in
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def  window_capture():
+    w = 3840 # set this
+    h = 1600 # set this
+    windowname = 'Steam'
+
+    hwnd = win32gui.FindWindow(None, windowname)
+
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (h, w, 4)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+    img = img[...,:3]
+
+    img = np.ascontiguousarray(img)
+
+    return img
+
 loop_time = time()
 while(True):
 
-    screenshot = ImageGrab.grab()
+    screenshot = window_capture()
     screenshot = np.array(screenshot)
     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
@@ -27,6 +58,5 @@ while(True):
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
         break
-
 
 print('Done.')
